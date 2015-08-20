31cd3c6efb0c4041d2ca8fd08b10b3fbb42f62ce
Konrad Bartecki <konradbartecki@outlook.com>
2015-08-20 08:00:20 +0200

    Init

---
+++ TaskOne.MobileService/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace TaskOne.MobileService.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
